c161da8d602cdb965d637660a4bc6257db7bd9f5
AlexDNZ <paleozene@gmail.com>
2023-02-10 22:05:44 +0100

    change user SQL table

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,11 @@
 CREATE TABLE "user" (
 	id SERIAL PRIMARY KEY,
-	first_name VARCHAR(255),
-	last_name VARCHAR(255),
-	email TEXT
+	login VARCHAR(255),
+	password TEXT,
+	email TEXT,
+	subscription_code VARCHAR(255) DEFAULT NULL,
+	validated BOOLEAN DEFAULT FALSE
 );
-INSERT INTO "user"(first_name, last_name, email) VALUES ('polo', 'martinezi', 'pmartinezi@yopmail.com');
-INSERT INTO "user"(first_name, last_name, email) VALUES ('jaky', 'etmichel', 'jakyetmichel@yopmail.com');
-INSERT INTO "user"(first_name, last_name, email) VALUES ('manu', 'tchao', 'manutchao@yopmail.com');
+
+INSERT INTO "user"(login, password, email) VALUES ('polo', 'qwerty', 'pmartinezi@yopmail.com');
+INSERT INTO "user"(login, password, email) VALUES ('manu', 'azerty', 'manutchao@yopmail.com');
