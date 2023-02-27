ead0373d9ed5c232127e626cf8081f310525086a
AlexDNZ <paleozene@gmail.com>
2023-02-27 21:49:20 +0100

    update db tables

--- db/init.sql
+++ db/init.sql
@@ -3,8 +3,10 @@ CREATE TABLE "user" (
 	login VARCHAR(255),
 	password TEXT,
 	email TEXT,
+	token VARCHAR(255), 
 	subscription_code VARCHAR(255) DEFAULT NULL,
-	validated BOOLEAN DEFAULT FALSE
+	validated BOOLEAN DEFAULT FALSE,
+	notified BOOLEAN DEFAULT FALSE
 );
 
 INSERT INTO "user"(login, password, email) VALUES ('polo', 'qwerty', 'pmartinezi@yopmail.com');
