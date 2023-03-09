055dc122b1c57070dd99e6e19b199c3b9b915ca4
AlexDNZ <paleozene@gmail.com>
2023-03-09 18:30:51 +0100

    prevent wrong file error

--- backend/server.rb
+++ backend/server.rb
@@ -30,29 +30,6 @@ elsif mode == "PROD"
 	console.puts "server launched on http://#{host}:#{port}"
 end
 
-
-def check_token(token, conn)
-	found = exist_by_value?("token",  token, "users", conn)
-	if found
-		user = find_by_value("token",  token, "users", conn)
-		return user
-	end
-	return nil
-end
-
-def get_blob(body)
-	first = body.enum_for(:scan, /(?=PNG)/).map do
-		Regexp.last_match.offset(0).first
-	end
-	last = body.enum_for(:scan, /(?=IEND)/).map do
-		Regexp.last_match.offset(0).first
-	end
-	first = first[0] - 1
-	last = last[0] + 7
-	body = body[first..last]
-	return body
-end
-
 i = 1
 loop do
 	client = server.accept
@@ -65,7 +42,7 @@ loop do
 	case [method_token, target.split('/')[1]]	
 		when ["GET", "data"]
 			response.status_code = "200 OK"
-			response.message =get_table_datas_with_users(conn, "pictures")
+			response.message = get_table_datas_with_users(conn, "pictures")
 		when ["POST", "register"]
 			all_headers = get_headers(client)
 			body = client.read(all_headers['Content-Length'].to_i)
@@ -155,21 +132,23 @@ loop do
 		when ["POST", "pictures"]
 			all_headers = get_headers(client)
 			body = client.read(all_headers['Content-Length'].to_i)
-			token = body.lines[1].split[2][6..-3]
-			user = check_token(token, conn)
-			puts user
-			if user	
-				body = get_blob(body)
-				puts "blob"
-				name = "blob_#{i}"
-				File.open("./upload/#{name}", 'wb') { |f| f.write body }
-				i+=1
-				hash = {:path => "http://#{host}:#{port}/pictures/#{name}", :content=> user["login"], :user_id=> user["id"]}
-				db_insert_request(conn, hash, "pictures")
-				response.status_code  = "201 Created"
-				response.message = JSON.generate({"success"=> "OK"})
-			else
-				response = forbidden_reponse(method_token, target, "invalid token")
+			begin
+				token = body.lines[1].split[2][6..-3]
+				user = check_token(token, conn)
+				if user	
+					body = get_blob(body)
+					name = "blob_#{i}"
+					File.open("./upload/#{name}", 'wb') { |f| f.write body }
+					i+=1
+					hash = {:path => "http://#{host}:#{port}/pictures/#{name}", :content=> user["login"], :user_id=> user["id"]}
+					db_insert_request(conn, hash, "pictures")
+					response.status_code  = "201 Created"
+					response.message = JSON.generate({"success"=> "OK"})
+				else
+					response = forbidden_reponse(method_token, target, "invalid token")
+				end
+			rescue
+				response = forbidden_reponse(method_token, target, "invalid blob file")
 			end
 		when ["GET", "pictures"]
 			pic_id = target.split('/')[2]
