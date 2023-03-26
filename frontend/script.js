91997620998112fe7581adfdbb4bdda7463478a4
AlexDNZ <paleozene@gmail.com>
2023-03-26 17:37:51 +0200

    set comment posting function

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,7 @@
 import { getHost, getCookie, getPartial } from '../config.js';
 import { setNavbar } from './components/navbar.js';
 import createCard from './components/pictureCard.js';
+import createCommentBox from './components/commentBox.js';
 
 document.addEventListener("DOMContentLoaded", async function(event) { 
 	
@@ -9,35 +10,15 @@ document.addEventListener("DOMContentLoaded", async function(event) {
 	await getPartial('navbar', './components/navbar.html')
 	.then(()=> {
 		setNavbar();
-		// const settingsBtn = document.getElementById("settingsBtn");
-		// const picBtn = document.getElementById("picBtn");
-		// const powerBtn = document.getElementById("powerBtn");
-
-		// if (getCookie("token") != "")
-		// {
-		// 	picBtn.style.display = "";
-		// 	settingsBtn.style.display = "";
-		// 	powerBtn.querySelector("svg").classList.add("text-white");
-		// 	powerBtn.status = "on";
-		// }
-
-
-		// powerBtn.addEventListener("click", () => 
-		// {
-		// 	if (powerBtn.status == "on")
-		// 	{
-		// 		document.cookie = "token" + "=";
-		// 		window.location.replace("./index.html");
-		// 	}
-		// 	else
-		// 	{
-		// 		window.location.replace("./connect.html");
-		// 	}
-		// });
 	})
 
-	
-	fetch(`http://${host}:1337/data`)
+	await getPictures(host);
+	getComments(host);
+});
+
+async function getPictures(host)
+{
+	await fetch(`http://${host}:1337/data`)
 	.then((response) => response.json())
 	.then((data) => 
 	{
@@ -45,15 +26,88 @@ document.addEventListener("DOMContentLoaded", async function(event) {
 		var list =  document.getElementById("dataList")
 		for (let el of data)
 		{			
-			// var item = createDataBox(el);
 			var item = createCard(el);
 			list.appendChild(item);
 			list.appendChild(document.createElement("br"));
+
+			const sendBtn = document.getElementById(`sendComment${el.id}`);
+			const comment = document.getElementById(`inputComment${el.id}`);
+			sendBtn.addEventListener("click", () => { postComment(host, comment, el.id) });	
 		}
 		return data;
 	})
-	.then((data) => {
-		// console.log(data);
+	.then((data) => {})
+}
+
+async function getComments(host)
+{
+	await fetch(`http://${host}:1337/comment`)
+	.then((response) => response.json())
+	.then((comments) => 
+	{
+		for (let comment of comments)
+		{		
+			let divId = `pictureCard${comment.picture_id}`	
+			var card = document.getElementById(divId);
+			if (!card)
+				continue;
+			var box = createCommentBox(comment);
+			var commentDiv = card.querySelector('.comments');
+			if (!commentDiv)
+				continue;
+			commentDiv.appendChild(box);
+		}
+		return comments;
 	})
-	
-});
+	.then((data) => {})
+}
+
+async function postComment(host, comment, id)
+{
+	let token = getCookie("token");
+	if (token == "")
+	{
+		alert("You should be connected to post comment");
+		console.error("no token found")
+		return;
+	}
+	const data = 
+	{
+		picture_id: parseInt(id, 10),
+		token: token,
+		content: comment.value
+	};
+
+	await fetch(`http://${host}:1337/comment`,
+	{
+		method: 'POST',
+		headers: {},
+		body: JSON.stringify(data),
+	})
+	.then((response) => 
+	{
+		if (response.ok)
+			return response.json();
+		else
+			return Promise.reject(response.json()); 
+	})
+	.then((res) => 
+	{
+		comment.value = "";
+		let divId = `pictureCard${res.picture_id}`	
+		var card = document.getElementById(divId);
+		if (!card)
+			return;
+		var box = createCommentBox(res);
+		var commentDiv = card.querySelector('.comments');
+		if (!commentDiv)
+			return;
+		commentDiv.appendChild(box);
+	})
+	.catch(async (err) => 
+	{
+		let error = await err;
+		console.error("error in posting comment: ");
+		console.error(error);
+	})
+}
