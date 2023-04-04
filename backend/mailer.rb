e21c272c608f4a9c6869c0ababd9bd1aea6371db
AlexDNZ <paleozene@gmail.com>
2023-04-04 19:37:33 +0200

    return status code

--- backend/mailer.rb
+++ backend/mailer.rb
@@ -13,9 +13,7 @@ def send_mail(_to, _subject, _content)
 
 	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
 	response = sg.client.mail._('send').post(request_body: mail.to_json)
-	puts response.status_code
+	return response.status_code
 	# puts response.body
 	# puts response.headers
 end
-
-
