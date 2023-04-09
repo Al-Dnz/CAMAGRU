1f44c2cdfa65104753fe0fc0220b7def5be57c1b
AlexDNZ <paleozene@gmail.com>
2023-04-09 19:10:25 +0200

    set to_http method inside class

--- backend/Response.rb
+++ backend/Response.rb
@@ -2,10 +2,25 @@ class Response
 	attr_accessor :status_code
 	attr_accessor :content_type 
 	attr_accessor :message
+	attr_accessor :cookie
 
 	def initialize(status_code: "", content_type: "application/json", message: "")
 		@status_code = status_code
 		@content_type = content_type
 		@message = message
+		@cookie = nil
+	end
+
+	def to_http(version_number, target)
+		http_response = <<~MSG
+			#{version_number} #{self.status_code}
+			Content-Type: #{self.content_type}; charset=#{self.message.encoding.name}
+			Access-Control-Allow-Origin: *
+			Location: #{target}
+			Set-Cookie: #{self.cookie}
+			
+			#{self.message}
+		MSG
+		return http_response 
 	end
 end
